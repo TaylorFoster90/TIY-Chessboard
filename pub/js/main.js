db9870f4f7f8bd0529b34845226dd0a39946f875
Evan Klemen <ejklemen@gmail.com>
2015-01-31 15:23:03 -0500

    trying as hard as we cab

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,9 +1,9 @@
-function print(board){
+function print(){
     document.write('<table>');
     
-    _.forEach(board, function(row, key){
+    _.forEach(board, function(row){
         document.write("<tr>");
-        _.forEach(row, function(square, k){
+        _.forEach(row, function(square){
             document.write("<td>" + square + "</td>")
    
         });
@@ -16,24 +16,24 @@ function print(board){
  
 var board = {};
 board.a = {
-    'one': '<span class="">&#9820;</span>',
-    'two': '<span class="">&#9822;</span>',
-    'three': '<span class="">&#9821;</span>',
-    'four': '<span class="">&#9819;</span>',
-    'five': '<span class="">&#9818;</span>',
-    'six': '<span class="">&#9821;</span>',
-    'seven': '<span class="">&#9822;</span>',
-    'eight': '<span class="">&#9820;</span>'
+    'one': '<span class="piece black rook"></span>',
+    'two': '<span class="piece black knight"></span>',
+    'three': '<span class="piece black bishop"></span>',
+    'four': '<span class="piece black king"></span>',
+    'five': '<span class="piece black queen"></span>',
+    'six': '<span class="piece black bishop"></span>',
+    'seven': '<span class="piece black knight"></span>',
+    'eight': '<span class="piece black rook"></span>'
 };
 board.b = {
-    'one': '&#9823;',
-    'two': '&#9823;',
-    'three': '&#9823;',
-    'four': '&#9823;',
-    'five': '&#9823;',
-    'six': '&#9823;',
-    'seven': '&#9823;',
-    'eight': '&#9823;'
+    'one': '<span class="piece black pawn"></span>',
+    'two': '<span class="piece black pawn"></span>',
+    'three': '<span class="piece black pawn"></span>',
+    'four': '<span class="piece black pawn"></span>',
+    'five': '<span class="piece black pawn"></span>',
+    'six': '<span class="piece black pawn"></span>',
+    'seven': '<span class="piece black pawn"></span>',
+    'eight': '<span class="piece black pawn"></span>'
 };
 board.c = {
     'one': '',
@@ -76,26 +76,26 @@ board.f = {
     'eight': ''
 };
 board.g = {
-    'one': '&#9823;',
-    'two': '&#9823;',
-    'three': '&#9823;',
-    'four': '&#9823;',
-    'five': '&#9823;',
-    'six': '&#9823;',
-    'seven': '&#9823;',
-    'eight': '&#9823;'
+    'one': '<span class="piece white pawn"></span>',
+    'two': '<span class="piece white pawn"></span>',
+    'three': '<span class="piece white pawn"></span>',
+    'four': '<span class="piece white pawn"></span>',
+    'five': '<span class="piece white pawn"></span>',
+    'six': '<span class="piece white pawn"></span>',
+    'seven': '<span class="piece white pawn"></span>',
+    'eight': '<span class="piece white pawn"></span>'
 };
 board.h = {
-    'one': '<span class="">&#9820;</span>',
-    'two': '<span class="">&#9822;</span>',
-    'three': '<span class="">&#9821;</span>',
-    'four': '<span class="">&#9819;</span>',
-    'five': '<span class="">&#9818;</span>',
-    'six': '<span class="">&#9821;</span>',
-    'seven': '<span class="">&#9822;</span>',
-    'eight': '<span class="">&#9820;</span>'
+    'one': '<span class="piece white rook"></span>',
+    'two': '<span class="piece white knight"></span>',
+    'three': '<span class="piece white bishop"></span>',
+    'four': '<span class="piece white king"></span>',
+    'five': '<span class="piece white queen"></span>',
+    'six': '<span class="piece white bishop"></span>',
+    'seven': '<span class="piece white knight"></span>',
+    'eight': '<span class="piece white rook"></span>'
 };
-
+ 
 print(board);
 //
 //// will house references to all pieces
